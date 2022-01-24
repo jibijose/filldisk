a268904ee76fdcf5f158c69cdc1c294e27f2f62f
Jibi Jose <jibijose@yahoo.com>
2022-01-24 10:50:18 +0530

    base springboot project

---
+++ src/main/java/com/jibi/filldisk/FillDiskApplication.java
@@ -0,0 +1,13 @@
+package com.jibi.filldisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FillDiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FillDiskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jibi/filldisk/FillDiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jibi.filldisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FillDiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
